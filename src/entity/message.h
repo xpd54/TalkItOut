6b9ddab36042afc275a0f59c908e7aa73995aa60
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-02-16 16:19:44 +0800

    Update message oid type

--- src/entity/message.h
+++ src/entity/message.h
@@ -1,5 +1,5 @@
 #pragma once
-#include <bsoncxx/oid.hpp>
+#include "../connection/mongo.h"
 #include <chrono>
 #include <string>
 namespace chat_box {
@@ -7,12 +7,13 @@ class Message {
 private:
   /* data */
 public:
-  Message(const bsoncxx::oid &message_id, const std::string &payload,
-          const bsoncxx::oid &user_id, const bsoncxx::oid &chat_room_id);
-  bsoncxx::oid message_id;
+  Message(const bsoncxx::types::b_oid &message_id, const std::string &payload,
+          const bsoncxx::types::b_oid &user_id,
+          const bsoncxx::types::b_oid &chat_room_id);
+  bsoncxx::types::b_oid message_id;
   std::string payload;
-  bsoncxx::oid user_id;
-  bsoncxx::oid chat_room_id;
+  bsoncxx::types::b_oid user_id;
+  bsoncxx::types::b_oid chat_room_id;
   std::chrono::system_clock::time_point time_stamp;
   void logDetails();
 };
