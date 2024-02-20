68809b6869239142a78361a5a5885dbe5ed1a23a
Ravi Prakash <raviprakash.xpd54@gmail.com>
2024-02-20 14:41:48 +0800

    Rename to generic object field

--- src/entity/message.h
+++ src/entity/message.h
@@ -8,12 +8,12 @@ private:
   /* data */
 public:
   Message(const bsoncxx::types::b_oid &message_id, const std::string &payload,
-          const bsoncxx::types::b_oid &user_id,
-          const bsoncxx::types::b_oid &chat_room_id);
+          const bsoncxx::types::b_oid &sender_id,
+          const bsoncxx::types::b_oid &destination_id);
   bsoncxx::types::b_oid message_id;
   std::string payload;
-  bsoncxx::types::b_oid user_id;
-  bsoncxx::types::b_oid chat_room_id;
+  bsoncxx::types::b_oid sender_id;
+  bsoncxx::types::b_oid destination_id;
   std::chrono::system_clock::time_point time_stamp;
   void logDetails();
 };
